4ec0883b025fba1d22ed42f47f829d158d213e6f
Michael Munje <michaelmunje@gmail.com>
2019-02-25 13:36:49 -0800

    Modify virtual environment scripts.

--- envs/setup.sh
+++ envs/setup.sh
@@ -1,2 +1,5 @@
 conda create -y -n astro-classify tensorflow-gpu pandas numpy
 conda create -y -n astro-classify-test tensorflow pandas numpy
+conda create -y -n astro-classify-mine pandas numpy
+source activate astro-classify-mine
+pip install astroquery
