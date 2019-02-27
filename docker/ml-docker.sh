4c789fbf5dffdac00c83b9eba4c233e47ec71ea9
Michael Munje <michaelmunje@gmail.com>
2019-02-27 15:42:47 -0800

    Update scripts to run dockerfiles.

--- docker/ml-docker.sh
+++ docker/ml-docker.sh
@@ -1,1 +1,1 @@
-docker run -d astro-classify
+docker run -ti --rm --net=host -v $(dirname "$(pwd)"):/astro-classify:rw -u `id -u $USER`:`id -g $USER` astro-classify
--- docker/test-docker.sh
+++ docker/test-docker.sh
@@ -1,1 +1,1 @@
-docker run -d astro-classify-test
+docker run -ti --rm --net=host -v $(dirname "$(pwd)"):/astro-classify:rw -u `id -u $USER`:`id -g $USER` astro-classify-test
