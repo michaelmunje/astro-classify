629ace5a31c2ea3722ea814fa3db496adafbac8f
Michael Munje <michaelmunje@gmail.com>
2019-02-27 14:07:15 -0800

    Added separate docker installation files in new subdirectory.

--- docker/install_docker.sh
+++
@@ -1,5 +0,0 @@
-docker build . -t astro-classify -f machine-learning/Dockerfile
-docker build . -t astro-classify-mine -f mine/Dockerfile
-docker build . -t astro-classify-test -f test/Dockerfile
-# to do: add cloud docker when necessary
-
