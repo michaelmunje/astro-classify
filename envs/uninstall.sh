e0afc34c6c0acd0392593ac46ec430f8855461cb
Michael Munje <michaelmunje@gmail.com>
2019-02-28 13:24:00 -0800

    Automated virtual environment uninstaller.

--- envs/uninstall.sh
+++ envs/uninstall.sh
@@ -1,4 +1,4 @@
-conda remove --name astro-classify-mine --all
-conda remove --name astro-classify --all
-conda remove --name astro-classify-test --all
+conda remove -y --name astro-classify-mine --all
+conda remove -y --name astro-classify --all
+conda remove -y --name astro-classify-test --all
 
