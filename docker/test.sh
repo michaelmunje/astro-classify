4040be954f21d8d5a545dffa3a6f3e8de4bb80fb
Michael Munje <michaelmunje@gmail.com>
2019-02-27 20:48:40 -0800

    Update docker run scripts to install if not built.

--- docker/test.sh
+++ docker/test.sh
@@ -1,1 +1,8 @@
+if [[ "$(docker images -q astro-classify-test 2> /dev/null)" == "" ]]; then
+cd Dockerfiles/install
+./install_test.sh
+cd ..
+fi
 docker run -ti --rm --net=host -v $(dirname "$(pwd)"):/astro-classify:rw -u `id -u $USER`:`id -g $USER` astro-classify-test bash -c "cd astro-classify; pytest"
+
+
