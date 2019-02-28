8d9641ce9eeb20c27da9daef64f083934b2127ca
Michael Munje <michaelmunje@gmail.com>
2019-02-28 15:12:22 -0800

    Add test for galaxy name extraction.

--- tests/extract_galaxy_names_test.py
+++ tests/extract_galaxy_names_test.py
@@ -1,12 +1,16 @@
 import pytest
 import astroclassify as ac
-import pandas as pd
 
 
 def test_galaxy_name_extraction():
     filename = "tests/test_cases/RAW_NASA_DATABASE_TABLE"
     df = ac.data.extract_galaxy_names(filename)
-    assert(len(list(df)) == 3)
+    print(df['names'])
+    assert(len(df['names']) == 3)
+    if (len(df['names']) == 3):
+        assert(df['names'][0] == "SOME GALAXY")
+        assert(df['names'][1] == "ANOTHER GALAXY")
+        assert(df['names'][2] == "LAST ONE")
 
 
 if __name__ == '__main__':
