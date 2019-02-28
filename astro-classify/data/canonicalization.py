a42a44a19ba3f5b7731b013e9d8218ad4c0252f8
Jonathan Kennedy <jekennedy8@gmail.com>
2019-02-28 15:23:50 -0800

    Setting up unit test for canonicalization

--- astro-classify/data/canonicalization.py
+++ astro-classify/data/canonicalization.py
@@ -1,23 +1,27 @@
-from os import listdir
+import os
 import sys
 
-def canonicalize(data_folder_path, output_path = ''):
+
+def canonicalize(data_folder_path, output_path=''):
     path = data_folder_path
-    files = listdir(path)
+    files = os.listdir(path)
     values = set()
 
     for file in files:
-        file_data = open(path + '\\' + file, 'r');
+        file_data = open(os.path.join(path, file), 'r')
         for line in file_data:
             if line.strip().strip('\n') != '':
                 value = line.strip('\n')
                 values.add(value)
     output_file = 'Master.txt'
     if output_path != '':
-        output_file = output_path + '\\' + output_file
+        output_file = os.path.join(output_path, output_file)
     write_file = open(output_file, 'w')
-    for value in values:
+    ordered_values = list(values)
+    ordered_values.sort()
+    for value in ordered_values:
         write_file.write(value + '\n')
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
