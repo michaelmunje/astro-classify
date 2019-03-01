bd7cb7479fc8e3225ad3331db8cd1f05f8d0fcc3
Michael Munje <michaelmunje@gmail.com>
2019-02-28 19:32:58 -0800

    Update merge and add to submodule init.

--- astroclassify/pipeline/__init__.py
+++ astroclassify/pipeline/__init__.py
@@ -1,1 +1,2 @@
 from .extract_galaxies import extract_galaxy_names
+from .merge import merge
--- astroclassify/pipeline/merge.py
+++ astroclassify/pipeline/merge.py
@@ -4,14 +4,15 @@ import sys
 
 def merge(data_folder_path, output_filepath):
     files = os.listdir(data_folder_path)
-    values = set()
+    values = list()
 
     for file in files:
         file_data = open(os.path.join(data_folder_path, file), 'r')
         for line in file_data:
             if line.strip().strip('\n') != '':
                 value = line.strip('\n')
-                values.add(value)
+                values.append(value)
+    values.sort()
     # if output_path is not valid, throw an exception
     write_file = open(output_filepath, 'w')
     for value in values:
