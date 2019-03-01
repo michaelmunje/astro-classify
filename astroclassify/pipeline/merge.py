c5005179323076df038e0b7c335717afa1cfe7cd
Michael Munje <michaelmunje@gmail.com>
2019-02-28 18:54:01 -0800

    Rename and update merge function.

--- astroclassify/pipeline/merge.py
+++ astroclassify/pipeline/merge.py
@@ -2,31 +2,24 @@ import os
 import sys
 
 
-def canonicalize(data_folder_path, output_path=''):
-    path = data_folder_path
-    files = os.listdir(path)
+def merge(data_folder_path, output_filepath):
+    files = os.listdir(data_folder_path)
     values = set()
 
     for file in files:
-        file_data = open(os.path.join(path, file), 'r')
+        file_data = open(os.path.join(data_folder_path, file), 'r')
         for line in file_data:
             if line.strip().strip('\n') != '':
                 value = line.strip('\n')
                 values.add(value)
-    output_file = 'Master.txt'
-    if output_path != '':
-        output_file = os.path.join(output_path, output_file)
-    write_file = open(output_file, 'w')
-    ordered_values = list(values)
-    ordered_values.sort()
-    for value in ordered_values:
+    # if output_path is not valid, throw an exception
+    write_file = open(output_filepath, 'w')
+    for value in values:
         write_file.write(value + '\n')
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        canonicalize(sys.argv[1])
-    elif len(sys.argv) == 3:
-        canonicalize(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 3:
+        merge(sys.argv[1], sys.argv[2])
     else:
-        raise ValueError('Expects an argument of the folder containing the data files')
+        raise ValueError('Expects a data folder path and output file path.')
