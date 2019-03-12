d0ee9ced9e9f60c376430208b99ecbd14e5745d9
Michael Munje <michaelmunje@gmail.com>
2019-03-11 22:00:52 -0700

    Fixed old names.

--- docker/Dockerfiles/uninstall/stop_all_containers.sh
+++ docker/Dockerfiles/uninstall/stop_all_containers.sh
@@ -1,3 +1,3 @@
-docker container stop $(docker ps -aq -f "ancestor=astro-classify" -f "ancestor=astro-classify-mine" -f "ancestor=astro-classify-test")
+docker container stop $(docker ps -aq -f "ancestor=galana" -f "ancestor=galana-mine" -f "ancestor=galana-test")
 # NOTE: Below line is not required because we have -rm in our docker launch options
 # docker container rm $(docker ps -aq -f "ancestor=astro-classify" -f "ancestor=astro-classify-mine" -f "ancestor=astro-classify-test")
