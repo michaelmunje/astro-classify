7f9304545c9afa0cf8e2697ce53aec7efbf0449c
Michael Munje <michaelmunje@gmail.com>
2019-03-23 15:43:43 -0700

    Fix table names extraction.

--- galana/mine/get_all_table_names.py
+++ galana/mine/get_all_table_names.py
@@ -9,8 +9,11 @@ xamin_script_loc = root_dir + "/utils/get_all_tables.sh"
 def get_script_output(script_location):
     process = subprocess.Popen(script_location, stdout=subprocess.PIPE)
     out, err = process.communicate()
-    return out.decode("utf-8")
+    table_list = out.decode("utf-8").split('\n')[1:-5]
+    clean_list = list((table_name.strip() for table_name in table_list))
+    return clean_list
 
 
 def get_all_tables():
+    os.chdir('utils')
     return get_script_output(xamin_script_loc)
