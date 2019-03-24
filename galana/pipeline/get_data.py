55853f0d2892e4a2bbb4e5eea1b611a7f6b77724
Michael Munje <michaelmunje@gmail.com>
2019-03-24 01:23:44 -0700

    Remove old code.

--- galana/pipeline/get_data.py
+++ galana/pipeline/get_data.py
@@ -3,26 +3,8 @@
 import pandas as pd
 import os
 from astroquery.ned import Ned 
+from .extract_galaxies import extract_galaxy_names
 
-def extract_galaxy_names(filename):
-    file = open(filename, 'r')
-    galaxy_names = []
-
-    data_found = False
-
-    for line in file.readlines():
-        if '|' in line and len(line) > 1:
-            s = line.split('|')[1].split('|')[0]
-            s = s.rstrip()
-            if (data_found):
-                galaxy_names.append(s)
-            else:
-                if (s == "name"):
-                    data_found = True
-
-    df = pd.DataFrame(galaxy_names, columns=['names'])
-
-    return df
 
 def retreive_tables(filepath):
     g_names = list(extract_galaxy_names(file_path)['names'])
@@ -34,11 +16,7 @@ def retreive_tables(filepath):
     
     return df_tables
 
-file_path = os.getcwd() + '/RAW_INPUT_DATA_CGMW'
-df_tables = retreive_tables(file_path)
 
-
-
-
-
-
+def get_cgmw():
+    file_path = os.getcwd() + '/RAW_INPUT_DATA_CGMW'
+    df_tables = retreive_tables(file_path)
