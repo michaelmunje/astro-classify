3dd6fb88325307ef290b3bf949ea848f6f13953e
Michael Munje <michaelmunje@gmail.com>
2019-03-24 00:20:40 -0700

    Add auto-reinstalls if old docker version.

--- docker/ml.sh
+++ docker/ml.sh
@@ -7,6 +7,24 @@ cd Dockerfiles/install
 cd ../..
 echo -e "\e[96mDocker image finished building."
 echo -e "\e[39m"
+cp Dockerfiles/machine-learning/Dockerfile Dockerfiles/current_installs/machine-learning/Dockerfile
+else
+	if cmp --silent Dockerfiles/machine-learning/Dockerfile Dockerfiles/current_installs/machine-learning/Dockerfile ; then
+	 echo -e "\e[96mLatest version detected..."
+	 echo -e "\e[39m"
+	else
+	 echo -e "\e[96mOld version detected. Installing new version..."
+	 echo -e "\e[39m"
+	 ./stop_all_containers.sh
+	 cd Dockerfiles/uninstall
+	 ./uninstall_ml.sh
+	 cd ../..
+	 cd Dockerfiles/install
+	 ./install_ml.sh
+	 cd ../..
+         rm Dockerfiles/current_installs/machine-learning/Dockerfile
+         cp Dockerfiles/machine-learning/Dockerfile Dockerfiles/current_installs/machine-learning/Dockerfile
+	fi
 fi
 echo -e "\e[96mRunning galana container..."
 echo -e "\e[39m"
