dcc87ef534e6dc241f6c7c6927a9e7b2daf5f00b
Michael Munje <michaelmunje@gmail.com>
2019-03-24 13:47:11 -0700

    Update imports and fix typos.

--- galana/preprocessing/get_data.py
+++ galana/preprocessing/get_data.py
@@ -1,22 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
 import pandas as pd
 import os
 from astroquery.ned import Ned 
 from .extract_data import extract_galaxy_names
 
 
-def retreive_tables(filepath):
-    g_names = list(extract_galaxy_names(file_path)['names'])
+def retrieve_tables(filepath):
+    g_names = list(extract_galaxy_names(filepath)['names'])
     result_tables = [Ned.query_object(g).to_pandas() for g in g_names]
     df_tables = pd.concat(result_tables, sort=False)
     df_tables = df_tables.dropna(axis=1)
     df_tables.head()
     df_tables = df_tables.drop(['No.'], axis=1)
-    
     return df_tables
 
 
 def get_cgmw():
     file_path = os.getcwd() + '/RAW_INPUT_DATA_CGMW'
-    df_tables = retreive_tables(file_path)
+    df_tables = retrieve_tables(file_path)
