b5e5d5bed2aca6090058c937ae4488d84bf03440
Michael Munje <michaelmunje@gmail.com>
2019-03-23 20:39:09 -0700

    Add try and except and recursive calls if a query fails.

--- galana/data/get_all_tables.py
+++ galana/data/get_all_tables.py
@@ -8,6 +8,15 @@ raw_data_dir = root_dir + '/data/mine/raw'
 abs_raw_data_path = os.path.abspath(raw_data_dir)
 
 
+def query_to_txt(filepath, command):
+    try:
+        with open(filepath, "w") as text_file:
+            text_file.write(get_inline_script_output(command))
+    except:
+        print("Failed getting data for " + '.'.split(('/'.split(filepath)[-1]))[0] + ". Trying again...")
+        query_to_txt(filepath, command)
+
+
 def run_table_query(table_name):
     print("Running query for " + table_name)
     filepath = abs_raw_data_path + '/' + table_name + ".txt"
@@ -15,8 +24,7 @@ def run_table_query(table_name):
     command = "java -jar xamin.jar table=" + table_name + " fields=Standard Format=aligned > '" + abs_raw_data_path \
               + '/' + table_name + ".txt'"
     filepath = abs_raw_data_path + '/' + table_name + ".txt"
-    with open(filepath, "w") as text_file:
-        text_file.write(get_inline_script_output(command))
+    query_to_txt(filepath, command)
 
 
 def get_all_tables():
