a68f1570442fe5abf24ecdbde53c31d5fcff447d
Michael Munje <michaelmunje@gmail.com>
2019-03-24 13:44:40 -0700

    Fix method to extract data. Add name extraction definition.

--- galana/preprocessing/extract_data.py
+++ galana/preprocessing/extract_data.py
@@ -1,22 +1,36 @@
 import pandas as pd
 
 
+def strip_list(input_list):
+    if input_list[0] == '':
+        input_list = input_list[1:]
+    if input_list[-1] == '':
+        input_list = input_list[:-1]
+    return input_list
+
+
 def extract_data(filepath):
     file = open(filepath, 'r')
-    galaxy_names = []
+    df = pd.DataFrame()
 
-    data_found = False
+    header_found = False
+    current_index = 0
 
     for line in file.readlines():
         if '|' in line and len(line) > 1:
-            s = line.split('|')[1].split('|')[0]
-            s = s.rstrip()
-            if data_found:
-                galaxy_names.append(s)
+            row = line.split('|')
+            row = list((value.strip() for value in row))
+            row = strip_list(row)
+            row = list((value if value != '' else 'null' for value in row))
+            if header_found is True:
+                df.loc[current_index] = row
+                current_index += 1
             else:
-                if s == "name":
-                    data_found = True
-
-    df = pd.DataFrame(galaxy_names, columns=['names'])
-
+                if header_found is False:
+                    header_found = True
+                    df = pd.DataFrame(columns=row)
     return df
+
+
+def extract_galaxy_names(filename):
+    return list(extract_data(filename)['name'])
