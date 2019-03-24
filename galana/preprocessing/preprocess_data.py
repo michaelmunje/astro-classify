30fef649d20170b76d1ab0f660279fa76682fd8d
Michael Munje <michaelmunje@gmail.com>
2019-03-24 13:32:16 -0700

    Fix typo.

--- galana/preprocessing/preprocess_data.py
+++ galana/preprocessing/preprocess_data.py
@@ -8,4 +8,4 @@ clean_dir = root_dir + "data/tables/clean"
 def preprocess_data(filepath):
     df = extract_data(filepath)
     filename = '/'.split(filepath)[-1]
-    df.to_csv(clean_dir + filename)
+    df.to_csv(clean_dir + '/' + filename)
