518858af0ae8e160de7bef555ff258ea15e6902e
Michael Munje <michaelmunje@gmail.com>
2019-03-24 00:20:40 -0700

    Add auto-reinstalls if old docker version.

--- docker/mine.sh
+++ docker/mine.sh
@@ -7,6 +7,24 @@ cd Dockerfiles/install
 cd ../..
 echo -e "\e[96mDocker image finished building."
 echo -e "\e[39m"
+cp Dockerfiles/mine/Dockerfile Dockerfiles/current_installs/mine/Dockerfile
+else
+	if cmp --silent Dockerfiles/mine/Dockerfile Dockerfiles/current_installs/mine/Dockerfile ; then
+	 echo -e "\e[96mLatest version detected..."
+	 echo -e "\e[39m"
+	else
+	 echo -e "\e[96mOld version detected. Installing new version..."
+	 echo -e "\e[39m"
+	 ./stop_all_containers.sh
+	 cd Dockerfiles/uninstall
+	 ./uninstall_mine.sh
+	 cd ../..
+	 cd Dockerfiles/install
+	 ./install_mine.sh
+	 cd ../..
+	 rm Dockerfiles/current_installs/mine/Dockerfile
+	 cp Dockerfiles/mine/Dockerfile Dockerfiles/current_installs/mine/Dockerfile
+	fi
 fi
 echo -e "\e[96mRunning galana-mine container..."
 echo -e "\e[39m"
--- docker/test.sh
+++ docker/test.sh
@@ -7,6 +7,24 @@ cd Dockerfiles/install
 cd ../..
 echo -e "\e[96mDocker image finished building."
 echo -e "\e[39m"
+cp Dockerfiles/test/Dockerfile Dockerfiles/current_installs/test/Dockerfile
+else
+	if cmp --silent Dockerfiles/test/Dockerfile Dockerfiles/current_installs/test/Dockerfile ; then
+	 echo -e "\e[96mLatest version detected..."
+	 echo -e "\e[39m"
+	else
+	 echo -e "\e[96mOld version detected. Installing new version..."
+	 echo -e "\e[39m"
+	 ./stop_all_containers.sh
+	 cd Dockerfiles/uninstall
+	 ./uninstall_test.sh
+	 cd ../..
+	 cd Dockerfiles/install
+	 ./install_test.sh
+	 cd ../..
+         rm Dockerfiles/current_installs/test/Dockerfile
+         cp Dockerfiles/test/Dockerfile Dockerfiles/current_installs/test/Dockerfile
+	fi
 fi
 echo -e "\e[96mRunning galana-test container..."
 echo -e "\e[39m"
