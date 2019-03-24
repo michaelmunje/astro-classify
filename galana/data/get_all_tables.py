7e7c2b9f0f920aa1ac273bae795dc4ca0d8506e9
Michael Munje <michaelmunje@gmail.com>
2019-03-23 19:48:25 -0700

    Fix table retrieval.

--- galana/data/get_all_tables.py
+++ galana/data/get_all_tables.py
@@ -1,24 +1,30 @@
-from multiprocessing import Pool
 import os
+from multiprocessing import Pool
 from data import get_all_tables_names
-import subprocess
 from helpers import get_inline_script_output
 
 root_dir = os.getcwd()
-data_dir = root_dir + '/data/mine/'
+raw_data_dir = root_dir + '/data/mine/raw'
+abs_raw_data_path = os.path.abspath(raw_data_dir)
 
 
 def run_table_query(table_name):
-    print("Running query for " + table_name )
-    print("Dir: " + os.getcwd())
-    command = "java -jar xamin.jar table=" + table_name + " fields=Standard Format=aligned > '../galana/data/tables/" \
-              + table_name + ".txt'"
-    get_inline_script_output(command)
+    print("Running query for " + table_name)
+    filepath = abs_raw_data_path + '/' + table_name + ".txt"
+    print("File to save to: " + filepath)
+    command = "java -jar xamin.jar table=" + table_name + " fields=Standard Format=aligned > '" + abs_raw_data_path \
+              + '/' + table_name + ".txt'"
+    filepath = abs_raw_data_path + '/' + table_name + ".txt"
+    with open(filepath, "w") as text_file:
+        text_file.write(get_inline_script_output(command))
 
 
 def get_all_tables():
+    print("Retrieving all table names...")
     table_names = get_all_tables_names()
+    print(table_names)
     os.chdir('utils')
-    p = Pool()
-    p.map(run_table_query, table_names)
+    print("Running all table queries....")
+    pool = Pool()
+    pool.map(run_table_query, table_names)
     os.chdir('..')
