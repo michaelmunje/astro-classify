66a7f8d87df841576ae2bc31d45f920e2bd3cb6e
Michael Munje <michaelmunje@gmail.com>
2019-03-23 21:06:27 -0700

    Seperate table retrieval into two different scripts.

--- galana/data/__init__.py
+++ galana/data/__init__.py
@@ -1,2 +1,3 @@
 from .get_all_table_names import get_all_tables_names
 from .get_all_tables import get_all_tables as get_all_raw
+from .get_table import run_table_query
--- galana/data/get_all_tables.py
+++ galana/data/get_all_tables.py
@@ -1,30 +1,7 @@
 import os
 from multiprocessing import Pool
 from ..data import get_all_tables_names
-from ..helpers import get_inline_script_output
-
-root_dir = os.getcwd()
-raw_data_dir = root_dir + '/data/mine/raw'
-abs_raw_data_path = os.path.abspath(raw_data_dir)
-
-
-def query_to_txt(filepath, command):
-    try:
-        with open(filepath, "w") as text_file:
-            text_file.write(get_inline_script_output(command))
-    except:
-        print("Failed getting data for " + '.'.split(('/'.split(filepath)[-1]))[0] + ". Trying again...")
-        query_to_txt(filepath, command)
-
-
-def run_table_query(table_name):
-    print("Running query for " + table_name)
-    filepath = abs_raw_data_path + '/' + table_name + ".txt"
-    print("File to save to: " + filepath)
-    command = "java -jar xamin.jar table=" + table_name + " fields=Standard Format=aligned > '" + abs_raw_data_path \
-              + '/' + table_name + ".txt'"
-    filepath = abs_raw_data_path + '/' + table_name + ".txt"
-    query_to_txt(filepath, command)
+from ..data import run_table_query
 
 
 def get_all_tables():
