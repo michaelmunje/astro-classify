579b6c1f68d97ec5bff718f0dd0e384c18bf28b9
Michael Munje <michaelmunje@gmail.com>
2019-03-24 14:01:32 -0700

    Add definition for preprocessing folder.

--- galana/preprocessing/preprocess_data.py
+++ galana/preprocessing/preprocess_data.py
@@ -5,7 +5,12 @@ root_dir = os.getcwd()
 clean_dir = root_dir + "data/tables/clean"
 
 
-def preprocess_data(filepath):
+def preprocess_data(filepath, output_path=clean_dir):
     df = extract_data(filepath)
     filename = '/'.split(filepath)[-1]
-    df.to_csv(clean_dir + '/' + filename)
+    df.to_csv(output_path + '/' + filename)
+
+
+def preprocess_folder(folderpath, output_path=clean_dir):
+    for filename in os.listdir(folderpath):
+        preprocess_data(filename, output_path)
