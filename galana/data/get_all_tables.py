f17fb99d9d3eaecb8dd6dffa7150786fe09ba29c
Michael Munje <michaelmunje@gmail.com>
2019-03-23 17:25:18 -0700

    Fix querying.

--- galana/data/get_all_tables.py
+++ galana/data/get_all_tables.py
@@ -5,19 +5,20 @@ import subprocess
 from helpers import get_inline_script_output
 
 root_dir = os.getcwd()
-data_dir = root_dir + 'data/mine/'
-table_script = root_dir + "/utils/get_table.sh"
+data_dir = root_dir + '/data/mine/'
+
+
+def run_table_query(table_name):
+    print("Running query for " + table_name )
+    print("Dir: " + os.getcwd())
+    command = "java -jar xamin.jar table=" + table_name + " fields=Standard Format=aligned > '../galana/data/tables/" \
+              + table_name + ".txt'"
+    get_inline_script_output(command)
 
 
 def get_all_tables():
-    print("")
+    table_names = get_all_tables_names()
     os.chdir('utils')
-    table_names =  get_all_tables_names()
-
     p = Pool()
     p.map(run_table_query, table_names)
     os.chdir('..')
-    
-def run_table_query(table_name):
-    command = "java -jar xamin.jar table=" + table_name + " fields=Standard Format=aligned > '../data/tables/" + table_name + ".txt'"
-    get_inline_script_output(command)
