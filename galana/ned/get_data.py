0b263034bab53e1f674b00f5200c45d41885a73f
Michael Munje <michaelmunje@gmail.com>
2019-03-24 20:53:05 -0700

    Comment out specific cgmw query.

--- galana/ned/get_data.py
+++ galana/ned/get_data.py
@@ -3,7 +3,7 @@ import os
 from astroquery.ned import Ned
 
 
-def retrieve_tables(filepath, galaxy_names):
+def query_object(galaxy_names):
     result_tables = [Ned.query_object(g).to_pandas() for g in galaxy_names]
     df_tables = pd.concat(result_tables, sort=False)
     df_tables = df_tables.dropna(axis=1)
@@ -11,7 +11,7 @@ def retrieve_tables(filepath, galaxy_names):
     df_tables = df_tables.drop(['No.'], axis=1)
     return df_tables
 
-
-def get_cgmw():
-    file_path = os.getcwd() + '/RAW_INPUT_DATA_CGMW'
-    df_tables = retrieve_tables(file_path)
+#
+# def get_cgmw():
+#     file_path = os.getcwd() + '/RAW_INPUT_DATA_CGMW'
+#     df_tables = retrieve_tables(file_path)
