212050d29e62ecf7c41076abb9c297baafc18f97
Michael Munje <michaelmunje@gmail.com>
2019-03-29 15:57:37 -0700

    Add function to train model.

--- galana/models/galaxy_classify.py
+++ galana/models/galaxy_classify.py
@@ -8,7 +8,15 @@ import numpy as np
 import os
 
 
+test_image_path = os.getcwd() + '/../../data/kaggle/images_test_rev1'
+test_image_files = os.listdir(test_image_path)
+train_image_path = os.getcwd() + '/../../data/kaggle/images_training_rev1'
+train_image_files = os.listdir(train_image_path)
+train_solutions = os.getcwd() + '/../../data/kaggle/training_solutions_rev1.csv'
+test_file = os.getcwd() + '/../../data/kaggle/all_zeros_benchmark.csv'
+
 df_headers = list()
+model = Model()
 
 
 def append_ext(fn):
@@ -45,80 +53,76 @@ def construct_model(df_headers):
         outputs.append(Dense(1, activation = 'sigmoid')(x))
     model = Model(inp,outputs)
     model.compile(optimizers.rmsprop(lr = 0.0001, decay = 1e-6),
-    loss = losses,metrics = ["accuracy"])
+    loss=losses,metrics=["accuracy"])
     
     return model
-#
-# test_image_path = os.getcwd() + '/../../data/kaggle/images_test_rev1'
-# test_image_files = os.listdir(test_image_path)
-# train_image_path = os.getcwd() + '/../../data/kaggle/images_training_rev1'
-# train_image_files = os.listdir(train_image_path)
-# train_solutions = os.getcwd() + '/../../data/kaggle/training_solutions_rev1.csv'
-# test_file = os.getcwd() + '/../../data/kaggle/all_zeros_benchmark.csv'
-#
-# traindf = pd.read_csv(train_solutions, dtype=str)
-# testdf = pd.read_csv(test_file, dtype=str)
-#
-# traindf["GalaxyID"] = traindf["GalaxyID"].apply(append_ext)
-# testdf["GalaxyID"] = testdf["GalaxyID"].apply(append_ext)
-# df_headers = list(traindf.columns)
-# test_headers = list(testdf.columns)
-#
-# datagen = IDG(rescale=1./255., validation_split=0.25)
-# test_datagen = IDG(rescale=1./255.)
-#
-# # Create generators
-# train_generator=datagen.flow_from_dataframe(
-#     dataframe=traindf,
-#     directory=train_image_path,
-#     x_col=df_headers[0],
-#     y_col=df_headers[1:],
-#     subset="training",
-#     batch_size=32,
-#     seed=42,
-#     shuffle=True,
-#     class_mode="other",
-#     target_size=(424,424))
-#
-# valid_generator=datagen.flow_from_dataframe(
-#     dataframe=traindf,
-#     directory=train_image_path,
-#     x_col=df_headers[0],
-#     y_col=df_headers[1:],
-#     subset="validation",
-#     batch_size=32,
-#     seed=42,
-#     shuffle=True,
-#     class_mode="other",
-#     target_size=(424,424))
-#
-# test_generator = test_datagen.flow_from_dataframe(
-#     dataframe=testdf,
-#     directory=test_image_path,
-#     x_col=test_headers[0],
-#     y_col=test_headers[1:],
-#     batch_size=32,
-#     seed=42,
-#     shuffle=False,
-#     class_mode=None,
-#     target_size=(424,424))
-#
-# model = construct_model(df_headers)
-#
-# # Train the model
-# STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
-# STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
-# STEP_SIZE_TEST=test_generator.n//test_generator.batch_size
-# model.fit_generator(generator=generator_wrapper(train_generator),
-#                     steps_per_epoch=STEP_SIZE_TRAIN,
-#                     validation_data=generator_wrapper(valid_generator),
-#                     validation_steps=STEP_SIZE_VALID,
-#                     epochs=1,verbose=2)
-#
-# # Predict Model
-# test_generator.reset()
-# pred = model.predict_generator(test_generator,
-#                               steps=STEP_SIZE_TEST,
-#                               verbose=1)
+
+
+def train_model():
+
+    traindf = pd.read_csv(train_solutions, dtype=str)
+    testdf = pd.read_csv(test_file, dtype=str)
+
+    traindf["GalaxyID"] = traindf["GalaxyID"].apply(append_ext)
+    testdf["GalaxyID"] = testdf["GalaxyID"].apply(append_ext)
+    df_headers = list(traindf.columns)
+    test_headers = list(testdf.columns)
+
+    datagen = IDG(rescale=1./255., validation_split=0.25)
+    test_datagen = IDG(rescale=1./255.)
+
+    # Create generators
+    train_generator=datagen.flow_from_dataframe(
+        dataframe=traindf,
+        directory=train_image_path,
+        x_col=df_headers[0],
+        y_col=df_headers[1:],
+        subset="training",
+        batch_size=32,
+        seed=42,
+        shuffle=True,
+        class_mode="other",
+        target_size=(424,424))
+
+    valid_generator=datagen.flow_from_dataframe(
+        dataframe=traindf,
+        directory=train_image_path,
+        x_col=df_headers[0],
+        y_col=df_headers[1:],
+        subset="validation",
+        batch_size=32,
+        seed=42,
+        shuffle=True,
+        class_mode="other",
+        target_size=(424,424))
+
+    test_generator = test_datagen.flow_from_dataframe(
+        dataframe=testdf,
+        directory=test_image_path,
+        x_col=test_headers[0],
+        y_col=test_headers[1:],
+        batch_size=32,
+        seed=42,
+        shuffle=False,
+        class_mode=None,
+        target_size=(424,424))
+
+    model = construct_model(df_headers)
+
+    # Train the model
+    STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
+    STEP_SIZE_VALID=valid_generator.n//valid_generator.batch_size
+    STEP_SIZE_TEST=test_generator.n//test_generator.batch_size
+    model.fit_generator(generator=generator_wrapper(train_generator),
+                        steps_per_epoch=STEP_SIZE_TRAIN,
+                        validation_data=generator_wrapper(valid_generator),
+                        validation_steps=STEP_SIZE_VALID,
+                        epochs=1,verbose=2)
+
+    # Predict Model
+    test_generator.reset()
+    pred = model.predict_generator(test_generator,
+                                  steps=STEP_SIZE_TEST,
+                                  verbose=1)
 
 
