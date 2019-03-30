d4b379e389084931e6e529c2f899d940cdeb8d97
Michael Munje <michaelmunje@gmail.com>
2019-03-30 01:22:03 -0700

    Small fix

--- stop.sh
+++ stop.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 docker container stop $(docker ps -aq -f "ancestor=galana")
-# NOTE: Below line is not required because we have -rm in our docker launch options
-# docker container rm $(docker ps -aq -f "ancestor=galana" -f "ancestor=galana-mine" -f "ancestor=galana-test")
