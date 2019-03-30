072a6505fdebbc7dfc88d6a0366b49b889b50d8f
Michael Munje <michaelmunje@gmail.com>
2019-03-30 01:10:01 -0700

    Simplify environments.

--- envs/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-conda create -y -n galana tensorflow-gpu pandas numpy sklearn
-conda create -y -n galana-test tensorflow pandas numpy pytest
-conda create -y -n galana-mine pandas numpy selenium
-source activate galana-mine
-pip install astroquery
-source deactivate
-source activate galana-test
-pip install astroquery
