350762623c65c3ca0a71d529f8746a3b04e16aaf
Michael Munje <michaelmunje@gmail.com>
2019-03-31 17:13:23 -0700

    Fix drop not using column names.

--- galana/models/galaxy_classify.py
+++ galana/models/galaxy_classify.py
@@ -22,12 +22,12 @@ model = Model()
 
 def read_galaxy_zoo(filepath):
     df = pd.read_csv(filepath, dtype=str)
-    df.drop('Class1.1', 'Class1.2', 'Class2.1', 'Class3.1', 'Class3.2', 'Class4.1',
+    df.drop(columns=['Class1.1', 'Class1.2', 'Class2.1', 'Class3.1', 'Class3.2', 'Class4.1',
             'Class4.2', 'Class5.2', 'Class5.3', 'Class5.4', 'Class7.1', 'Class7.2',
             'Class7.3', 'Class8.1', 'Class8.2', 'Class8.3', 'Class8.4', 'Class8.5',
             'Class8.6', 'Class8.7', 'Class9.1', 'Class9.2', 'Class9.3', 'Class10.1',
             'Class10.2', 'Class10.3', 'Class11.1', 'Class11.2', 'Class11.3', 'Class11.4',
-            'Class11.5', 'Class11.6')
+            'Class11.5', 'Class11.6'])
     df.rename(index=str, columns={"Class1.3": "Star", "Class2.2": "Spiral", "Class6.1": "Irregular",
                                   "Class6.2": "Elliptical"})
     return df
