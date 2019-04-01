3b3aac4beb7d4c02867f5137d8476cb257cafaaf
Michael Munje <michaelmunje@gmail.com>
2019-03-31 18:43:59 -0700

    Update galaxy zoo preprocessor.

--- galana/models/galaxy_classify.py
+++ galana/models/galaxy_classify.py
@@ -21,15 +21,26 @@ model = Model()
 
 
 def read_galaxy_zoo(filepath):
-    df = pd.read_csv(filepath, dtype=str)
-    df.drop(columns=['Class1.1', 'Class1.2', 'Class2.1', 'Class3.1', 'Class3.2', 'Class4.1',
-            'Class4.2', 'Class5.2', 'Class5.3', 'Class5.4', 'Class7.1', 'Class7.2',
+    df = pd.read_csv(filepath)
+
+    df['GalaxyID'] = df['GalaxyID'].astype(str)
+
+    df['Spiral'] = df['Class1.2'] * df['Class2.2']
+    df['Irregular'] = df['Class6.1'] * (df['Class1.1'] + (df['Class1.2'] * df['Class2.1']))
+    df['Elliptical'] = df['Class6.2'] * (df['Class1.1'] + (df['Class1.2'] * df['Class2.1']))
+
+    df['Other'] = 1 - df['Elliptical'] - df['Irregular'] - df['Spiral']
+
+    df = df.drop(columns=['Class1.1', 'Class1.2', 'Class1.3', 'Class2.1',
+            'Class2.2', 'Class3.1', 'Class3.2', 'Class4.1',
+            'Class4.2', 'Class5.1', 'Class5.2', 'Class5.3', 'Class5.4',
+            'Class6.1', 'Class6.2', 'Class7.1', 'Class7.2',
             'Class7.3', 'Class8.1', 'Class8.2', 'Class8.3', 'Class8.4', 'Class8.5',
             'Class8.6', 'Class8.7', 'Class9.1', 'Class9.2', 'Class9.3', 'Class10.1',
             'Class10.2', 'Class10.3', 'Class11.1', 'Class11.2', 'Class11.3', 'Class11.4',
             'Class11.5', 'Class11.6'])
-    df.rename(index=str, columns={"Class1.3": "Star", "Class2.2": "Spiral", "Class6.1": "Irregular",
-                                  "Class6.2": "Elliptical"})
+
+    print(df.head)
     return df
 
 
