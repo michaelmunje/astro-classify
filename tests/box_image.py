53106b50e93dfdee53af47e3b22b28241f14388e
Daniel Tellier <telldanieljames@gmail.com>
2019-04-13 12:27:27 -0700

    crops out black space around image

--- tests/box_image.py
+++ tests/box_image.py
@@ -5,7 +5,7 @@ import numpy as np
 image_path = os.getcwd() + '/252342.jpg'
 
 # Minimum percentage of pixels of same hue to consider dominant colour
-MIN_PIXEL_CNT_PCT = (1.0/20.0)
+MIN_PIXEL_CNT_PCT = (1.0/5.0)
 
 image = cv2.imread(image_path)
 if image is None:
@@ -20,16 +20,17 @@ bins = np.bincount(h.flatten())
 peaks = np.where(bins > (h.size * MIN_PIXEL_CNT_PCT))[0]
 
 print(peaks)
-exit(0)
+# exit(0)
 
 # Now let's find the shape matching each dominant hue
 for i, peak in enumerate(peaks):
+    peak = np.array(peak, dtype='uint8')
     # First we create a mask selecting all the pixels of this hue
     mask = cv2.inRange(h, peak, peak)
     # And use it to extract the corresponding part of the original colour image
     blob = cv2.bitwise_and(image, image, mask=mask)
 
-    _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for j, contour in enumerate(contours):
         bbox = cv2.boundingRect(contour)
@@ -37,20 +38,20 @@ for i, peak in enumerate(peaks):
         contour_mask = np.zeros_like(mask)
         cv2.drawContours(contour_mask, contours, j, 255, -1)
 
-        print("Found hue %d in region %s." % (peak, bbox))
+        # print("Found hue %d in region %s." % (peak, bbox))
         # Extract and save the area of the contour
         # region = blob.copy()[bbox[1]:bbox[1]+bbox[3],bbox[0]:bbox[0]+bbox[2]]
         # region_mask = contour_mask[bbox[1]:bbox[1]+bbox[3],bbox[0]:bbox[0]+bbox[2]]
         # region_masked = cv2.bitwise_and(region, region, mask=region_mask)
-        # file_name_section = "colourblobs-%d-hue_%03d-region_%d-section.png" % (i, peak, j)
+        # file_name_section = os.getcwd() + "/bbox/colourblobs-%d-hue_%03d-region_%d-section.jpg" % (i, peak, j)
         # cv2.imwrite(file_name_section, region_masked)
         # print(" * wrote '%s'" % file_name_section)
 
-        # # Extract the pixels belonging to this contour
-        # result = cv2.bitwise_and(blob, blob, mask=contour_mask)
-        # # And draw a bounding box
-        # top_left, bottom_right = (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3])
-        # cv2.rectangle(result, top_left, bottom_right, (255, 255, 255), 2)
-        # file_name_bbox = "colourblobs-%d-hue_%03d-region_%d-bbox.png" % (i, peak, j)
-        # cv2.imwrite(file_name_bbox, result)
-        # print(" * wrote '%s'" % file_name_bbox)
+        # Extract the pixels belonging to this contour
+        result = cv2.bitwise_and(blob, blob, mask=contour_mask)
+        # And draw a bounding box
+        top_left, bottom_right = (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3])
+        cv2.rectangle(result, top_left, bottom_right, (255, 255, 255), 2)
+        file_name_bbox = os.getcwd() + "/bbox/colourblobs-%d-hue_%03d-region_%d-bbox.jpg" % (i, peak, j)
+        cv2.imwrite(file_name_bbox, result)
+        print(" * wrote '%s'" % file_name_bbox)
