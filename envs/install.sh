5d4d7e1f3d766f980ff024ab44754bc62a0c9299
Michael Munje <michaelmunje@gmail.com>
2019-04-29 12:42:43 -0700

    Add amazon api to virtual env

--- envs/install.sh
+++ envs/install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 conda create -y --name galana
 source activate galana
-conda install -y --quiet --yes \
+conda install --quiet --yes \
     'numpy' \
     'pandas' \
-    'tensorflow-gpu' \
     'pytest' \
+    'keras' \
     'pytest-cov' \
     'matplotlib' \
-    'keras' \
-    'pillow'
+    'pillow' \
+    'jupyter' \
+    'tensorflow-gpu'
+pip install 'jupyter-tensorboard'
+pip install 'boto3'
 conda clean -tipsy
-pip install pytest-cov
-conda install -y -c astropy astroquery
-pip uninstall -y keras-preprocessing
+conda install -c --yes astropy astroquery
+#apt-get install -y libsm6 libxext6 libxrender-dev && \
+pip uninstall -y keras-preprocessing && \
 pip install git+https://github.com/keras-team/keras-preprocessing.git
