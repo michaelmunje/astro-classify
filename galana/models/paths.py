dcf32b3b1c061f1bf7524cfa7cf828d7a5bd7733
Michael Munje <michaelmunje@gmail.com>
2019-05-04 15:30:23 -0700

    Separate validation data. Only augment training data.

--- galana/models/paths.py
+++ galana/models/paths.py
@@ -9,22 +9,33 @@ class ModelPaths:  # Quick hack to make struct-like objects in python
 
 def initialize_default_paths():
     model_paths = ModelPaths()
-    model_paths.test_image_path = os.getcwd() + '/data/kaggle/images_test_rev1/'
+    model_paths.test_image_path = os.getcwd() + '/data/kaggle/test_images/'
     model_paths.train_image_path = os.getcwd() + '/data/kaggle/images_training_rev1/'
+    model_paths.valid_image_path = os.getcwd() + '/data/kaggle/valid_images/'
 
     if os.path.isfile(model_paths.test_image_path):
         model_paths.test_image_files = os.listdir(model_paths.test_image_path)
     else:
         model_paths.test_image_files = []
 
+    if os.path.isfile(model_paths.valid_image_path):
+        model_paths.valid_image_files = os.listdir(model_paths.test_image_path)
+    else:
+        model_paths.valid_image_files = []
+
     if os.path.isfile(model_paths.train_image_path):
         model_paths.train_image_files = os.listdir(model_paths.train_image_path)
     else:
         model_paths.train_image_files = []
 
-    model_paths.train_solutions = os.getcwd() + '/data/kaggle/solutions/training_solutions_rev1.csv'
-    model_paths.clean_train_solutions = os.getcwd() + '/data/kaggle/solutions/clean_solutions.csv'
-    model_paths.augmented_solutions = os.getcwd() + '/data/kaggle/solutions/augmented_solutions.csv'
+    model_paths.all_solutions = os.getcwd() + '/data/kaggle/solutions/training_solutions_rev1.csv'
+    model_paths.clean_solutions = os.getcwd() + '/data/kaggle/solutions/clean_solutions.csv'
+
+    model_paths.clean_train_solutions = os.getcwd() + '/data/kaggle/solutions/train_clean_solutions.csv'
+    model_paths.augmented_train_solutions = os.getcwd() + '/data/kaggle/solutions/train_augmented_solutions.csv'
+
+    model_paths.valid_solutions = os.getcwd() + '/data/kaggle/solutions/valid_solutions.csv'
+    model_paths.test_solutions = os.getcwd() + '/data/kaggle/solutions/test_solutions.csv'
 
     model_paths.test_file = os.getcwd() + '/data/kaggle/all_zeros_benchmark.csv'
 
@@ -45,6 +56,7 @@ def initialize_default_paths():
 
     pl.Path(model_paths.test_image_path).mkdir(parents=True, exist_ok=True)
     pl.Path(model_paths.train_image_path).mkdir(parents=True, exist_ok=True)
+    pl.Path(model_paths.valid_image_path).mkdir(parents=True, exist_ok=True)
     pl.Path(os.getcwd() + '/data/kaggle/models/').mkdir(parents=True, exist_ok=True)
     pl.Path(os.getcwd() + '/data/kaggle/solutions/').mkdir(parents=True, exist_ok=True)
     pl.Path(os.getcwd() + '/data/kaggle/eval/valid/').mkdir(parents=True, exist_ok=True)
@@ -53,12 +65,13 @@ def initialize_default_paths():
     return model_paths
 
 
-def initialize_custom_paths(test_images_p, train_images_p, train_sol, clean_sols, augmented_sols,
+def initialize_custom_paths(test_images_p, valid_images_p, train_images_p, train_sol, clean_sols, augmented_sols, valid_sols, test_sols,
                             test_f, output_model_f, output_model_w, checkpoint_p_outer, checkpoint_p_overall, conf_matrix,
                             other_metrics, val_true, val_preds, test_preds, test_true):
     model_paths = ModelPaths()
     model_paths.test_image_path = test_images_p
     model_paths.train_image_path = train_images_p
+    model_paths.valid_image_path = valid_images_p
 
     if os.path.isfile(model_paths.test_image_path):
         model_paths.test_image_files = os.listdir(model_paths.test_image_path)
@@ -70,9 +83,17 @@ def initialize_custom_paths(test_images_p, train_images_p, train_sol, clean_sols
     else:
         model_paths.train_image_files = []
 
-    model_paths.train_solutions = train_sol
+        if os.path.isfile(model_paths.valid_image_path):
+            model_paths.valid_image_files = os.listdir(model_paths.test_image_path)
+        else:
+            model_paths.valid_image_files = []
+
+    model_paths.all_solutions = train_sol
     model_paths.clean_train_solutions = clean_sols
-    model_paths.augmented_solutions = augmented_sols
+    model_paths.augmented_train_solutions = augmented_sols
+    model_paths.valid_solutions = valid_sols
+    model_paths.test_solutions = test_sols
+
     model_paths.test_file = test_f
     model_paths.output_model_file = output_model_f
     model_paths.output_model_weights = output_model_w
