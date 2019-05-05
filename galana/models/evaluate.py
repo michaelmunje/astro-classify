16609f1c34d4b49d6b46e51cdac2f20b252f7ddc
Michael Munje <michaelmunje@gmail.com>
2019-05-05 00:57:26 -0700

    Further debugging

--- galana/models/evaluate.py
+++ galana/models/evaluate.py
@@ -5,12 +5,9 @@ import pandas as pd
 def calc_conf_matrix(y_pred, y_actual):
     conf_matrix = np.zeros((3, 3), dtype=np.int8)
 
-    print(conf_matrix)
-
     for pred_row, actual_col in zip(y_pred, y_actual):
         conf_matrix[pred_row, actual_col] += 1
-
-    print(conf_matrix)
+        print(conf_matrix)
 
     return conf_matrix
 
